f2fd74703fcd5f96d65549dbea4e5e2d25585cfe
Anselmo Muller <anselmomuller@gmail.com>
2015-07-29 01:32:18 -0300

    Enviando fontes para repositorio

---
+++ app/src/main/java/com/example/anselmomuller/mymoney/MenuPrincipal.java
@@ -0,0 +1,68 @@
+package com.example.anselmomuller.mymoney;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MenuPrincipal extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_principal);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_menu_principal, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void banco(View v){
+        Intent i = new Intent(this,Banco.class);
+        startActivity(i);
+    }
+
+    public void fontePagadora(View v){
+        Intent i = new Intent(this,FontePagadora.class);
+        startActivity(i);
+    }
+
+    public void credor(View v){
+        Intent i = new Intent(this,Credor.class);
+        startActivity(i);
+    }
+    public void contaPagar(View v){
+        Intent i = new Intent(this,ContaPagar.class);
+        startActivity(i);
+    }
+
+    public void contaReceber(View v){
+        Intent i = new Intent(this,ContaReceber.class);
+        startActivity(i);
+    }
+
+    // Button button = (Button) findViewById(R.id.imageButton);
+
+}
